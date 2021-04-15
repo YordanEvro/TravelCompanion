88e1ce224d9eafc215f1221fd9b1a70d74c24c98
Nirmal <nirmal_864@hotmail.com>
2021-04-15 16:31:52 -0400

    added sidebar

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -4,6 +4,7 @@ import ReactTooltip from "react-tooltip";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import MapChart from "../components/MapChart";
+import SideBar from "../components/Sidebar";
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../css/styles.css';
@@ -13,6 +14,7 @@ function App() {
   const [content, setContent] = useState("");
   return (
     <div>
+      <SideBar />
       <MapChart setTooltipContent={setContent} />
       <ReactTooltip>{content}</ReactTooltip>
     </div>
