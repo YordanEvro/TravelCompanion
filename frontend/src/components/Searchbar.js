c887198c8e5ebad14a7457c7ada1b3d20e147ce5
Nirmal <nirmal_864@hotmail.com>
2021-04-15 19:06:38 -0400

    using simple search form

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -3,12 +3,19 @@ import React from 'react';
 
 const Searchbar = (props) => {
     return (
-        <div id = "searchBar" >
+    <div id = "searchBar" >
+    <form>
     <input type = 'search'
     className = 'search'
-    placeholder ="Enter country, place"
+    placeholder ="Search country, amenity"
     onChange = {props.handleChange}
     />
+    <select id="type" name="types">
+    <option value="country">Country</option>
+    <option value="amenities">Amenity</option>
+  </select>
+    <input type="submit" value= "search"/>
+    </form>
     </div>
     )
 
