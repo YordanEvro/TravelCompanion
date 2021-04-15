8f0f80e15ca595d808a1f0ebce6ce9099c23b860
Nirmal <nirmal_864@hotmail.com>
2021-04-15 19:40:49 -0400

    material ui search bar

--- frontend/src/components/Searchbar.js
+++ frontend/src/components/Searchbar.js
@@ -1,26 +1,21 @@
 import React from 'react';
-
+import SearchBar from "material-ui-search-bar";
+import { useState } from "react";
 
 const Searchbar = (props) => {
+
+    const [values, setValues] = useState("");
     return (
-    <div id = "searchBar" >
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"/>
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
-    <form>
-    <input type = 'text'
-    className = 'search'
-    placeholder ="Search country, amenity"
-    onChange = {props.handleChange}
-    />
-    <select id="type" name="types">
-    <option value="country">Country</option>
-    <option value="amenities">Amenity</option>
-  </select>
-  <button type="submit" class="searchButton">
-        <i class="fa fa-search"></i>
-        </button>
-    </form>
-    </div>
+        <div id = "searchBar">
+        <SearchBar
+        onChange={(newValue) => setValues({ values: newValue })}
+        onRequestSearch={() => console.log('onRequestSearch')}
+        style={{
+            margin: '0 auto',
+            maxWidth: 500
+          }}
+      />
+      </div>
     )
 
 }
